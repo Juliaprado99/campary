bfbd19560088d4465a19eed132a5b382639d6d26
Julia do Prado <juliedprado>
2022-03-28 12:43:09 -0300

    primeiro commit

---
+++ verificar.js
@@ -0,0 +1,16 @@
+function verificar() {
+    let nome = document.getElementById("nome");
+    let email = document.getElementById("email");
+    let tel = document.getElementById("tel");
+    let insert = document.getElementById("insert");
+    if (nome.value.length == 0 || email.value.length < 5 || tel < 5) {
+        insert.innerHTML = (`Verifique seu email e tente novamente.`);
+        insert.style.color = "red";
+
+    } else {
+        insert.innerHTML = (`Seu cadastro foi realizado!`);
+        insert.style.color = "green";
+
+
+    }
+}
